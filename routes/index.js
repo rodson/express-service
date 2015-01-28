ff9d7858f3cad8d08a287cc8a9ae342e449e0244
rodson <rodsonluo@gmail.com>
2015-01-28 16:19:10 +0800

    Add get todos api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var todos = [
+  "Learn polymer",
+  "Learn matieral design",
+  "Learn Angularjs"
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/todos', function(req, res, next) {
+  res.json(todos);
 });
 
 module.exports = router;
